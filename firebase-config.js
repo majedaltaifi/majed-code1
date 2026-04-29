30fff7917b7371549b16b7d13597c3fa8ae80db5
majedaltaifi <153317619+majedaltaifi@users.noreply.github.com>
2026-04-29 09:54:31 +0300

    Fix: Implemented strict Whitelist-based RBAC and resolved data leak race conditions in HR Portal and Dashboard

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = { 
+    apiKey: "AIzaSyCWW5fJIYSw0p_LGuDJZA-4bOfQ92tdUPM", 
+    authDomain: "majed-code1.firebaseapp.com", 
+    projectId: "majed-code1", 
+    storageBucket: "majed-code1.firebasestorage.app", 
+    messagingSenderId: "668474846040", 
+    appId: "1:668474846040:web:8ddd1c4818cda34e97d32d", 
+    measurementId: "G-HJKQWYHMHZ" 
+};
